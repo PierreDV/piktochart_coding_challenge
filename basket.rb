59fe059e0b5acb67d0f392e94baf518f7e4d579f
PierreDV <p.verteuil@gmail.com>
2025-10-26 17:26:32 +0100

    Add delivery charge rules

--- basket.rb
+++ basket.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Basket
-  attr_reader :catalogue, :items
+  attr_reader :catalogue, :delivery_charge_rules, :items
 
-  def initialize(catalogue:)
+  def initialize(catalogue:, delivery_charge_rules:)
     @catalogue = catalogue
+    @delivery_charge_rules = delivery_charge_rules.sort_by { |charge_rule| charge_rule[:cost_under] }
     @items = []
   end
 
@@ -17,10 +18,16 @@ class Basket
   end
 
   def total
-    items.sum { |item| price_for(item) }.floor(2)
+    items_total = items.sum { |item| price_for(item) }.floor(2)
+    items_total + delivery_charge(items_total)
   end
 
   private
+
+  def delivery_charge(items_total)
+    applicable_rule = delivery_charge_rules.find { |rule| items_total < rule[:cost_under] }
+    applicable_rule ? applicable_rule[:charge] : 0
+  end
 
   def valid_product_code?(product_code)
     catalogue.any? { |catalogue_item| catalogue_item[:product_code] == product_code }
