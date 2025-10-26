34b2da69518bf917c5d6980e9b651b1aa3f7d1b6
Peter de Verteuil <p.verteuil@gmail.com>
2025-10-26 16:07:03 +0100

    Add catalogue and prevent items with invalid product codes from being addded to basket
    * Add catalogue attribute to Basket class.
    * Do not allow items to be added to Basket if product code is not included in catalogue.

--- basket.rb
+++ basket.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 class Basket
-  attr_reader :items
+  attr_reader :catalogue, :items
 
-  def initialize
+  def initialize(catalogue:)
+    @catalogue = catalogue
     @items = []
   end
 
   def add(product_code)
+    unless valid_product_code?(product_code)
+      raise ArgumentError, "Product code #{product_code} is not included in the catalogue."
+    end
+
     items << product_code
+  end
+
+  private
+
+  def valid_product_code?(product_code)
+    catalogue.any? { |catalogue_item| catalogue_item[:product_code] == product_code }
   end
 end
--- basket_test.rb
+++ basket_test.rb
@@ -5,7 +5,12 @@ require_relative 'basket'
 
 describe 'Basket' do
   def setup
-    @basket = Basket.new
+    @catalogue = [
+      { product_code: 'R01', price: 32.95 },
+      { product_code: 'G01', price: 24.95 },
+      { product_code: 'B01', price: 7.95 }
+    ]
+    @basket = Basket.new(catalogue: @catalogue)
   end
 
   describe '#initialize' do
@@ -18,6 +23,11 @@ describe 'Basket' do
     it 'adds a product to the basket' do
       @basket.add('B01')
       _(@basket.items).must_equal ['B01']
+    end
+
+    it 'raises an error if product code is not included in catalogue' do
+      error = _ { @basket.add('NOGOOD') }.must_raise ArgumentError
+      _(error.message).must_equal 'Product code NOGOOD is not included in the catalogue.'
     end
   end
 end
